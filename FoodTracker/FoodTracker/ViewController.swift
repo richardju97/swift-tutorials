bb87e52539aeaa606c5e61b90bc208e210ea830c
rju <rju@purdue.edu>
2017-03-14 16:10:00 -0400

    Removed setDefaultTextLabel button; connected accessibility features.

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,9 +58,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 
     
 //    MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
 //        Hide the Keyboard.
 //        Ensures that if the image is tapped while the user typing in the text field, the keyboard is dismissed properly.
