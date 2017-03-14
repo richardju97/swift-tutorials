fab00dd3106d7fde53eaa7db4b46170df852f2bf
rju <rju@purdue.edu>
2017-03-14 06:18:13 -0400

    Initial commit of RatingControl.swift

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Richard Ju on 3/14/17.
+//
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
