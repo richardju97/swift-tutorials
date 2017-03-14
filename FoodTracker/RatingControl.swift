c77cdc817c322a8ed36b2a28ab2d01d97c324827
rju <rju@purdue.edu>
2017-03-14 06:40:43 -0400

    Modified to create 5 buttons; All buttons stored in an array; rating variable instantiated at 0.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class RatingControl: UIStackView {
 
+//    MARK: Properties
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
 //    MARK: Initialization
     
     override init(frame: CGRect) {
@@ -31,19 +35,24 @@ class RatingControl: UIStackView {
 //    MARK: Private Methods
     private func setupButtons() {
         
+        for _ in 0..<5 {
 //        Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
         
 //        Add Constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
         
 //        Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         
 //        Add the button to the stack
-        addArrangedSubview(button)
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
 }
