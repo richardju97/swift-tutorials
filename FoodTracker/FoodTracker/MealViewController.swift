911bfbe0e05f0461236a0cf0541bf093c2a89afc
rju <rju@purdue.edu>
2017-03-15 06:51:25 -0400

    Renamed ViewController.swift to MealViewController.swift

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Richard Ju on 3/13/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 //    MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
