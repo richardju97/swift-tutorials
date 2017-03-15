2332160cbc74153dcd777423d42be4f7cd0a28c7
rju <rju@purdue.edu>
2017-03-14 22:46:03 -0400

    Initial commit of MainTableViewCell.swift

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Richard Ju on 3/14/17.
+//
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
