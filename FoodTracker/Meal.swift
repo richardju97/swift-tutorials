280094073d4af54c715e1bc022a24d6d286f6623
rju <rju@purdue.edu>
2017-03-17 04:36:28 -0400

    Implemented encoding/decoding with NSCoder/NSObject, created a file path to data for persisting data.

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import os.log
 
 // name = string, photo = UIImage (optional), rating = int
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     var name: String
@@ -29,4 +30,43 @@ class Meal {
         self.photo = photo
         self.rating = rating
     }
+    
+    //MARK: Types
+    struct PropertyKey {
+        
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+        
+    }
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        //Name is required - Initializer should fail if we cannot decode a name string
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            
+            os_log("Unable to decode the name for a Meal object", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        //Photo is optional - therefore use conditional cast
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        //Rating is an integer - therefore no need to downcast
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        //Must call designated initalizer
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
 }
